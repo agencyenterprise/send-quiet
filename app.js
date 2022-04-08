c208ddb3a6f891a74855c24b3602e9b1f6a03462
Glitch (hello-express) <none>
2022-04-08 15:41:41 +0000

    🌎📅 Checkpoint ./app.js:43681989/865

--- app.js
+++ app.js
@@ -38,44 +38,14 @@ app.event('app_home_opened', async ({ event, client, context }) => {
   }
 });
 
-app.command('/sendq', async ({ ack, payload, context }) => {
-  console.log("sendq received");
+app.command('/sendq', async ({ ack, payload, context, respond }) => {
   // Acknowledge the command request
   ack();
 
-  try {
-    const result = await app.client.chat.postMessage({
-      token: context.botToken,
-      // Channel to send message to
-      channel: payload.channel_id,
-      // Include a button in the message (or whatever blocks you want!)
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: 'Go ahead. Click it.'
-          },
-          accessory: {
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: 'Click me!'
-            },
-            action_id: 'button_abc'
-          }
-        }
-      ],
-      // Text in the notification
-      text: 'Message from Test App'
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
+    
+  
+  await respond("test");
 });
-
 
 (async () => {
   // Start your app

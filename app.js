f5fcd16e291629742f535f720bdfffac59672a28
Glitch (hello-express) <none>
2022-04-08 15:29:41 +0000

    📡🚉 Checkpoint ./app.js:43681989/16964 ./examples/actions.js:43681989/1654

--- app.js
+++ app.js
@@ -8,65 +8,78 @@ const app = new App({
 
 
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-  
-  
-  app.event('app_home_opened', async ({ event, client, context }) => {
-  try {
-    console.log('')
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
+app.event('app_home_opened', async ({ event, client, context }) => {
+    try {
+      console.log('event.user: ', event.user);
+      /* view.publish is the method that your app uses to push a view to the Home tab */
+      const result = await client.views.publish({
 
-      /* the user that opened your app's app home */
-      user_id: event.user,
+        /* the user that opened your app's app home */
+        user_id: event.user,
 
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
+        /* the view object that appears in the app home*/
+        view: {
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Hello *World*"
               }
-            ]
-          }
-        ]
+            }
+          ]
+        }
       }
-    });
-  }
-  catch (error) {
+    );
+    
+  } catch (error) {
     console.error(error.data);
   }
 });
-  
+
+app.command('/sendq', async ({ ack, payload, context }) => {
+  console.log("sendq received");
+  // Acknowledge the command request
+  ack();
+
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
   
   console.log('⚡️ Bolt app is running!');
 })();
